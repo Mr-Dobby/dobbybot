017d22698702ad1dd884488417cadc5e1b67c7eb
Mr Dobby <rasmusbejder041200@gmail.com>
2021-02-02 23:10:07 +0100

    Update mod_clear.js

--- commands/Moderation/mod_clear.js
+++ commands/Moderation/mod_clear.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args, client) => {
   const Sucess = bot.emojis.cache.get("697388354668462110");
 
   var noPermsEmbed = new Discord.MessageEmbed()
-      .setDescription(`${Failure} Clearing messages requires you to have \`ADMINISTRATOR\` permissions.`)
+      .setDescription(`${Failure} Clearing messages requires you to have \`MANAGE MESSAGES\` permissions.`)
       .setColor("#ff0000")
     
   var noPermsEmbedBot = new Discord.MessageEmbed()
@@ -30,42 +30,61 @@ module.exports.run = async (bot, message, args, client) => {
 
   if (logchannel) {
 
-          let clearErrorEmbed = new Discord.MessageEmbed()
-              .setColor("#ff4f4f")
-              .setTitle(`\`Command: ${currPrefix.prefix}clear\` | Alias: \`purge\``)
-              .addField("**Description:**", "Deletes message in this channel. Max 100, newer than 2 weeks.")
-              .addField("**Command usage:**", `${currPrefix.prefix}clear <Number>`)
-              .addField("**Example:**", `${currPrefix.prefix}clear 69`)
-              .setFooter("<> = Required, [] = Optional")
+    let clearErrorEmbed = new Discord.MessageEmbed()
+      .setColor("#ff4f4f")
+      .setTitle(`\`Command: ${currPrefix.prefix}clear\` | Alias: \`purge\``)
+      .addField("**Description:**", "Deletes messages. Channel or user, max 100.")
+      .addField("**Command usage:**", `${currPrefix.prefix}clear <Number> [@User/#Channel]`)
+      .addField("**Example:**", `${currPrefix.prefix}clear 69 #general`)
+      .setFooter("<> = Required, [] = Optional")
 
-              if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-                return message.channel.send(clearErrorEmbed)
-              }
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+      return message.channel.send(clearErrorEmbed)
+    }
 
-              let deleteCount;
+    let deleteCount;
 
-              if (parseInt(args[0]) > 100) {
-                deleteCount = 100;
-              } else { 
-                deleteCount = parseInt(args[0]);
-              }
+    if (parseInt(args[0]) > 100) {
+        deleteCount = 100;
+      } else { 
+        deleteCount = parseInt(args[0]);
+    }
 
-              // Deleting the messages
-              message.delete()
-              await message.channel.bulkDelete(deleteCount, true)
-              .catch(err => message.channel.send(`Error: ${err}\nTry again.`))
-        
-              const clearEmbed = new Discord.MessageEmbed()
-                    .setAuthor(`${message.author.tag} | Clear`, message.author.displayAvatarURL({ dynamic: true }))
-                    .setDescription(`\`${currPrefix.prefix}clear <Number>\``)
-                    .setColor("#4fff7f")
-                    .addField("Cleared", `${deleteCount} messages`, true)
-                    .addField("Channel", `${message.channel}`, true)
-                    .addField("Moderator", `<@${message.author.id}>`, true)
-                    .setFooter(`ID: ${message.author.id}`)
-                    .setTimestamp();
-        
-              logchannel.send(clearEmbed)
+    let MemberOrChannel = bot.users.cache.get(args[1]) || bot.channels.cache.get(args[1]) || message.guild.member(message.mentions.users.first()) || message.guild.channels.cache.get(args[1]) || message.channel.id;
+
+    let clearEmbed = new Discord.MessageEmbed()
+    .setAuthor(`${message.author.tag} | Clear`, message.author.displayAvatarURL({ dynamic: true }))
+    .setColor("#4fff7f")
+    .addField("Cleared", `${deleteCount} messages`, true)
+    .addField("Moderator", `<@${message.author.id}>`, true)
+    .setFooter(`ID: ${message.author.id}`)
+    .setTimestamp();
+
+    message.delete()
+
+    try {
+      if (bot.guilds.cache.get(message.guild.id).member(MemberOrChannel)) {
+        message.channel.messages.fetch({ limit: 100 }).then(messages => {
+          const filter = MemberOrChannel ? MemberOrChannel.id : bot.user.id;
+          messages = messages.filter(m => m.author.id == filter).array().slice(0, deleteCount);
+          message.channel.bulkDelete(messages + 1, true).catch(e => {
+            message.channel.send(`Something went wrong: ${e}`)
+          })
+        })
+        clearEmbed.setDescription(`\`${currPrefix.prefix}clear <number> <@user>\``).addField(`Member`, `${MemberOrChannel.toString()}`, true)
+        logchannel.send(clearEmbed)
+      } else {
+        if (message.channel.type == "text") {
+          message.channel.bulkDelete(deleteCount + 2, true).catch(e => {
+            message.channel.send(`Something went wrong: ${e}`)
+          })
+        }
+        clearEmbed.setDescription(`\`${currPrefix.prefix}clear <number> <#channel>\``).addField(`Channel`, `<#${MemberOrChannel}>`, true)
+        logchannel.send(clearEmbed)
+      }
+    } catch (e) {
+      message.channel.send(`Something went wrong: ${e}`)
+    }
 
   } else {
 
@@ -77,9 +96,22 @@ module.exports.run = async (bot, message, args, client) => {
     .addField("**Example:**", `${currPrefix.prefix}clear 69`)
     .setFooter("<> = Required, [] = Optional")
 
+ 
+    var noPermsEmbed = new Discord.MessageEmbed()
+        .setDescription(`${Failure} Clearing messages requires you to have \`MANAGE MESSAGES\` permissions.`)
+        .setColor("#ff0000")
+  
+    var noPermsEmbedBot = new Discord.MessageEmbed()
+        .setDescription(`${Failure} Clearing messages requires me to have \`MANAGE MESSAGES\` permissions.`)
+        .setColor("#ff0000")
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+      return message.channel.send(noPermsEmbedBot)
+    }
+
     if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-      return message.channel.send('You need `MANAGE MESSAGES` permission')
-    };
+      return message.channel.send(noPermsEmbed);
+    }
 
     if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
       return message.channel.send(clearErrorEmbed)
